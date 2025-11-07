b9c7b2074659e9ad5ac0a6e372a227c092687eae
Yago Henner <yago.doria@grupofasitec.com.br>
2025-11-07 18:01:44 -0300

    feat: Implementa entidade de Produto e o Enum do Status

---
+++ Domain/Entities/Produto.cs
@@ -0,0 +1,73 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities;
+
+public class Produto
+{
+    public Guid Id { get; private set; }
+    public string Sku { get; private set; }
+    public string Nome { get; private set; }
+    public string Descricao { get; private set; }
+    public decimal Preco { get; private set; }
+    public string Categoria { get; private set; }
+    public StatusProdutoEnum Status { get; private set; }
+    public string? UrlFoto { get; private set; }
+    public DateTime GeradoEm { get; private set; }
+    public DateTime AtualizadoEm { get; private set; }
+
+    private Produto() { }
+
+    public Produto(string sku, string nome, string descricao, decimal preco, string categoria)
+    {
+        Id = Guid.NewGuid();
+        Sku = sku;
+        Nome = nome;
+        Descricao = descricao;
+        Preco = preco;
+        Categoria = categoria;
+        Status = StatusProdutoEnum.Ativo;
+        GeradoEm = DateTime.UtcNow;
+        AtualizadoEm = DateTime.UtcNow;
+
+        ValidarProduto();
+    }
+
+    public void Update(string name, string description, decimal price, string category)
+    {
+        Nome = name;
+        Descricao = description;
+        Preco = price;
+        Categoria = category;
+        AtualizadoEm = DateTime.UtcNow;
+
+        ValidarProduto();
+    }
+
+    public void Desativar()
+    {
+        Status = StatusProdutoEnum.Inativo;
+        AtualizadoEm = DateTime.UtcNow;
+    }
+
+    public void Ativar()
+    {
+        Status = StatusProdutoEnum.Ativo;
+        AtualizadoEm = DateTime.UtcNow;
+    }
+
+    public void AtribuirUrlFoto(string url)
+    {
+        UrlFoto = url;
+        AtualizadoEm = DateTime.UtcNow;
+    }
+
+    private void ValidarProduto()
+    {
+        if (string.IsNullOrWhiteSpace(Sku))
+            throw new ArgumentException("SKU é obrigatório.");
+        if (string.IsNullOrWhiteSpace(Nome))
+            throw new ArgumentException("Nome é obrigatório.");
+        if (Preco <= 0)
+            throw new ArgumentException("Preço deve ser positivo.");
+    }
+}
---
+++ Domain/Enums/StatusProdutoEnum.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Enums;
+
+public enum StatusProdutoEnum
+{
+    [Display(Name = "Inativo")]
+    Inativo,
+
+    [Display(Name = "Ativo")]
+    Ativo
+}
