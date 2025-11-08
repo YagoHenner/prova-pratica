6ad02fb1f0debae12e82cb57786280e1dfaecfb4
YagoHenner <yagohenner@gmail.com>
2025-11-08 16:48:58 -0300

    fix: Corrige importação de método na interface

--- Application/Interfaces/Repositories/IProdutoRepository.cs
+++ Application/Interfaces/Repositories/IProdutoRepository.cs
@@ -5,6 +5,7 @@ namespace Application.Interfaces.Repositories;
 
 public interface IProdutoRepository
 {
+    Task<Produto?> Obter(Guid id, CancellationToken cancellationToken = default);
     Task<Produto?> ObterPorCodigo(string codigo, CancellationToken cancellationToken = default);
     Task Criar(Produto produto, CancellationToken cancellationToken = default);
     void Editar(Produto produto);
